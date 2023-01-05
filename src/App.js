270355b82a3f86f92b81284f8089cb691e7db6d8
mhr1380 <mhr.1380.2001@gmail.com>
2023-01-05 18:59:51 +0330

    first commit.everything is solid! as soon as possible i will make it dynmaic.

--- src/App.js
+++ src/App.js
@@ -1,24 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "./App.css";
+import axios from "axios";
+import { useEffect, useState } from "react";
 function App() {
+  const [tickets, setTickets] = useState([]);
+  const [counter, setCounter] = useState(0);
+  const getTickets = async () => {
+    const { data } = await axios.get(
+      "https://hostservice.raja.ir/Api/ServiceProvider/TrainListEq?q=wFLgz7OBdr6i/yEvaG5RvKohbTplyVsAmT08uG7lOBvK2ZmViP0yqi2WcJ/PyoS4RN1NIF7CQixkZbFgpDauWEWsRhApsVSOj+NZMusWE08="
+    );
+    const { GoTrains } = data;
+    const filteredTickets = GoTrains.filter(
+      (ticket) => ticket.Counting > 0 && ticket.ExitTime === "15:00"
+    );
+    if (filteredTickets.length > 0) {
+      const audio = new Audio("https://www.soundjay.com/buttons/beep-01a.wav");
+      audio.play();
+    }
+    setTickets(GoTrains);
+  };
+  useEffect(() => {
+    if (counter === 0) {
+      getTickets();
+      setCounter(counter + 1);
+    }
+    const timeOutId = setTimeout(() => {
+      // const audio = new Audio(""); //https://www.soundjay.com/buttons/beep-01a.wav
+      // audio.play();
+      getTickets();
+      setCounter(counter + 1);
+    }, 20000);
+    return () => {
+      clearTimeout(timeOutId);
+    };
+  }, [counter]);
+  const renderTickets = () => {
+    const availableTickets = tickets.filter((ticket) => ticket.Counting > 0);
+    return availableTickets.map((ticket, index) => {
+      return (
+        <div key={index} className="ticket">
+          <div>{ticket.ExitTime} </div>
+          <div>{ticket.TimeOfArrival}</div>
+          <div>{ticket.Counting}</div>
+          <div>{ticket.FullPrice}</div>
+        </div>
+      );
+    });
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div>
+        {tickets.length > 0 ? renderTickets() : ""} {counter}
+      </div>
+    </>
   );
 }
 
